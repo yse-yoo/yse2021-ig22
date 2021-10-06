cb182b16b2626e8ece9c8630fef92dd1efda9143
yse-yoo <youhei_yoshikawa@yse-c.net>
2021-10-06 09:35:56 +0900

    入荷データ表示作成

--- nyuka.php
+++ nyuka.php
@@ -14,26 +14,46 @@
  * ①session_status()の結果が「PHP_SESSION_NONE」と一致するか判定する。
  * 一致した場合はif文の中に入る。
  */
-if (/* ①.の処理を行う */) {
+if (session_status() === PHP_SESSION_NONE) {
 	//②セッションを開始する
+	session_start();
 }
 
-
 //③SESSIONの「login」フラグがfalseか判定する。「login」フラグがfalseの場合はif文の中に入る。
-if (/* ③の処理を書く */){
+if (isset($_SESSION['login']) && $_SESSION['login']){
 	//④SESSIONの「error2」に「ログインしてください」と設定する。
+	$_SESSION['error2'] = 'ログインしてください';
 	//⑤ログイン画面へ遷移する。
+	header('Location: login.php');
 }
 
 //⑥データベースへ接続し、接続情報を変数に保存する
-
 //⑦データベースで使用する文字コードを「UTF8」にする
+$db_name = 'zaiko2021_yse';
+$db_host = 'localhost';
+$db_port = '3306';
+$db_user = 'zaiko2021_yse';
+$db_password = '2021zaiko';
+
+$dsn = "mysql:dbname={$db_name};host={$db_host};charset=utf8;port={$db_port}";
+try {
+	$pdo = new PDO($dsn, $db_user, $db_password);
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+} catch (PDOException $e) {
+	echo "接続失敗: " . $e->getMessage();
+	exit;
+}
 
 //⑧POSTの「books」の値が空か判定する。空の場合はif文の中に入る。
-if(/* ⑧の処理を行う */){
-	//⑨SESSIONの「success」に「入荷する商品が選択されていません」と設定する。
-	//⑩在庫一覧画面へ遷移する。
-}
+// if(empty($_POST['books'])){
+// 	//⑨SESSIONの「success」に「入荷する商品が選択されていません」と設定する。
+// 	$_SESSION['success'] = '入荷する商品が選択されていません';
+// 	//⑩在庫一覧画面へ遷移する。
+// 	header('Location: zaiko_ichiran.php');
+// }
+
+$books = fetchBooks($_POST['books'], $pdo);
 
 function getId($id,$con){
 	/* 
@@ -41,8 +61,20 @@ function getId($id,$con){
 	 * その際にWHERE句でメソッドの引数の$idに一致する書籍のみ取得する。
 	 * SQLの実行結果を変数に保存する。
 	 */
+	$sql = "SELECT * FROM books WHERE id = {$id}";
+	$stmt = $con->query($sql);
 
 	//⑫実行した結果から1レコード取得し、returnで値を返す。
+	return $stmt->fetch(PDO::FETCH_ASSOC);
+}
+
+function fetchBooks($ids, $pdo){
+	$id = implode(',', $ids);
+	$condition = "id in ($id)";
+	$sql = "SELECT * FROM books WHERE {$condition}";
+	$stmt = $pdo->query($sql);
+
+	return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 
 ?>
@@ -77,9 +109,9 @@ function getId($id,$con){
 			 * ⑬SESSIONの「error」にメッセージが設定されているかを判定する。
 			 * 設定されていた場合はif文の中に入る。
 			 */ 
-			if(/* ⑬の処理を書く */){
-				//⑭SESSIONの「error」の中身を表示する。
-			}
+			// if(/* ⑬の処理を書く */){
+			// 	//⑭SESSIONの「error」の中身を表示する。
+			// }
 			?>
 			</div>
 			<div id="center">
@@ -95,26 +127,20 @@ function getId($id,$con){
 							<th id="in">入荷数</th>
 						</tr>
 					</thead>
-					<?php 
-					/*
-					 * ⑮POSTの「books」から一つずつ値を取り出し、変数に保存する。
-					 */
-    				foreach(/* ⑮の処理を書く */){
-    					// ⑯「getId」関数を呼び出し、変数に戻り値を入れる。その際引数に⑮の処理で取得した値と⑥のDBの接続情報を渡す。
-					?>
-					<input type="hidden" value="<?php echo	/* ⑰ ⑯の戻り値からidを取り出し、設定する */;?>" name="books[]">
+					 <!-- ⑮POSTの「books」から一つずつ値を取り出し、変数に保存する。
+    				 ⑯「getId」関数を呼び出し、変数に戻り値を入れる。その際引数に⑮の処理で取得した値と⑥のDBの接続情報を渡す。 -->
+    				<?php foreach($books as $book): ?>
+					<input type="hidden" value="<?= $id ?>" name="books[]">
 					<tr>
-						<td><?php echo	/* ⑱ ⑯の戻り値からidを取り出し、表示する */;?></td>
-						<td><?php echo	/* ⑲ ⑯の戻り値からtitleを取り出し、表示する */;?></td>
-						<td><?php echo	/* ⑳ ⑯の戻り値からauthorを取り出し、表示する */;?></td>
-						<td><?php echo	/* ㉑ ⑯の戻り値からsalesDateを取り出し、表示する */;?></td>
-						<td><?php echo	/* ㉒ ⑯の戻り値からpriceを取り出し、表示する */;?></td>
-						<td><?php echo	/* ㉓ ⑯の戻り値からstockを取り出し、表示する */;?></td>
+						<td><?= $book['id'] ?></td>
+						<td><?= $book['title'] ?></td>
+						<td><?= $book['author'] ?></td>
+						<td><?= $book['salesDate'] ?></td>
+						<td><?= $book['price'] ?></td>
+						<td><?= $book['stock'] ?></td>
 						<td><input type='text' name='stock[]' size='5' maxlength='11' required></td>
 					</tr>
-					<?php
-					 }
-					?>
+					<?php endforeach ?>
 				</table>
 				<button type="submit" id="kakutei" formmethod="POST" name="decision" value="1">確定</button>
 			</div>
